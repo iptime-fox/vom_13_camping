2ac7e1b2dbe1d226642003224630c037842c4c25
iptime-fox <dudndi826@gmail.com>
2023-05-25 23:07:10 +0900

    main page 0525

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*------ Recommended Slider ------*/
+
+const scdSwiper = new Swiper('.swiper', {
+  loop: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesPerView: 3.5,
+  spaceBetween: 10,
+});
