98e5a9ee03b76e2e4c0189fd903579caeb3a515e
iptime-fox <dudndi826@gmail.com>
2023-05-27 00:30:03 +0900

    map page 0527

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 /*------ Recommended Slider ------*/
 
-const scdSwiper = new Swiper('.swiper', {
+const recSwiper = new Swiper('.recommended-swiper', {
   loop: true,
   autoplay: true,
   autoplaySpeed: 2000,
   slidesPerView: 3.5,
   spaceBetween: 10,
+});
+
+const mapSwiper = new Swiper('.map-swiper', {
+  loop: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesPerView: 1,
 });
